64a8e4c8d548d6cedd33efe35aef0947b3693f56
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2024-06-12 18:17:59 +0000

    initial commit

---
+++ tests/wrappertest.py
@@ -0,0 +1,40 @@
+import sys 
+sys.path.append("..")
+from ezr import adds, SYM, div
+import random, string, os
+
+def task4():
+  #str = ''.join(random.choices(string.ascii_lowercase +
+  #                             string.digits, k=10))
+  str = '''
+    -a --any     #todo's to explore             = 100
+    -c --cohen   size of the Cohen d            = 0.35
+    -d --decs    #decimals for showing floats   = 3
+    -e --enough  want cuts at least this good   = 0.1
+    -F --Far     how far to seek faraway        = 0.8
+    -h --help    show help                      = False
+    -H --Half    #rows for searching for poles  = 128
+    -k --k       bayes low frequency hack #1    = 1
+    -l --label   initial number for labelling    = 4
+    -L --Last    max allow #labelling            = 30
+    -m --m       bayes low frequency hack #2    = 2
+    -n --n       tinyN                          = 12
+    -N --N       smallN                         = 0.5
+    -p --p       distance function coefficient  = 2
+    -R --Run     start up action method         = help
+    -s --seed    random number seed             = 1234567891
+    -t --train   training data                  = data/misc/auto93.csv
+    -T --test    test data (defaults to train)  = None
+    -v --version show version                   = False
+    -x --xys     max #bins in discretization    = 16
+
+  '''
+  sym = adds(SYM(),[c for c in str])
+  print(str, div(sym), sep=" ")
+
+
+def main():
+  task4()
+
+if __name__=="__main__":
+  main()
